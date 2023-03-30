e4419c789924054c133389d53eb0f8e608befa7a
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2023-03-30 08:46:04 -0300

    avance

--- Persona.cs
+++ Persona.cs
@@ -1,5 +1,28 @@
-Class Persona {
+class Persona {
     private int dni{get;set;}
+    private DateTime fechaNacimiento{get;set;}
+    public string nombre{get;set;}
+    public string apellido{get;set;}
+    public string email{get;set;}
+    private int edad{get;set;}
 
-        
+    public Persona(int DNI, string NOMBRE, string APELLIDO, DateTime FNAC, string EMAIL){
+        dni = DNI; nombre = NOMBRE; apellido = APELLIDO; fechaNacimiento = FNAC; email = EMAIL;
+        edad = MiEdad();
+    }
+    public Persona(){
+
+    }
+    private int MiEdad()
+    {
+        DateTime FechaNacimientoHoy = new DateTime(DateTime.Today.Year, fechaNacimiento.Month, fechaNacimiento.Day);
+        if (FechaNacimientoHoy< DateTime.Today)  edad = DateTime.Today.Year - fechaNacimiento.Year;
+            else   edad = DateTime.Today.Year - fechaNacimiento.Year -1;
+        return edad; 
+    }
+    public bool PuedeVotar(){
+        edad = MiEdad();
+        return (edad >= 16);
+    }
+    
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+
+List<Persona> listaPersonas = new List<Persona>();
+
+int opcion;
+do
+{
+    Console.Clear();
+    Console.WriteLine("1. Ingresar Alumno");
+    Console.WriteLine("2. Ver Lista de Alumnos");
+    Console.WriteLine("3. Salir");
+    opcion = Funciones.IngresarEnteroEnRango("Elija opción: ", 1, 3);
+    Console.Clear();
+    switch (opcion)
+    {
+        case 1:
+            IngresarAlumno();
+            break;
+        case 2:
+            VerLista();
+            break;
+    }
+} while (opcion != 3);
+
